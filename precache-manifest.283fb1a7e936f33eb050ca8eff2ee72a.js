d2b2bcf9ea4acae5da23236895ca9a253402aeef
Salvatore <dested@gmail.com>
2020-02-26 21:56:45 -0700

    Updates

---
+++ precache-manifest.283fb1a7e936f33eb050ca8eff2ee72a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8eedecd86f17a67f40090c58e4fb7b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddd2214926cef0613950",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "0e37f8e461511de82703",
+    "url": "/static/js/2.740d6f68.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.740d6f68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddd2214926cef0613950",
+    "url": "/static/js/main.3bc7efac.chunk.js"
+  },
+  {
+    "revision": "c0eaecdd3fd6e8e8a2e1",
+    "url": "/static/js/runtime-main.d89ad71b.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
